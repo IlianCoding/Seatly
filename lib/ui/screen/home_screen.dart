f939c4b3df666484c2c6d007b336929c6d5f5f7e
IlianCoding <ilian.coding@gmail.com>
2024-09-17 10:31:23 +0200

    feat: Setting up the HomeScreen

---
+++ lib/ui/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class HomeScreen extends HookWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
