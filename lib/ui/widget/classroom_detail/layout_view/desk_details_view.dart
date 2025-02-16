583f0725f11d5d3b7f6fa621005e0fd8b48cfb1a
ilianCoding <ilian.coding@gmail.com>
2025-02-16 22:21:00 +0100

    feat: Adding the DeskDetailWidget.

--- lib/ui/widget/classroom_detail/layout_view/desk_details_view.dart
+++ lib/ui/widget/classroom_detail/layout_view/desk_details_view.dart
@@ -1,17 +1,35 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:seatly/domain/desk.dart';
 import 'package:seatly/domain/student.dart';
+import 'package:seatly/ui/providers/viewmodel_providers.dart';
+import 'package:seatly/ui/viewmodel/classroom_detailpage_viewmodel.dart';
 
-class DeskDetailWidget extends StatelessWidget {
+class DeskDetailWidget extends StatefulWidget {
   final Desk desk;
-  final Student student;
 
-  const DeskDetailWidget({super.key, required this.desk, required this.student});
+  const DeskDetailWidget({super.key, required this.desk});
+
+  @override
+  _DeskDetailWidgetState createState() => _DeskDetailWidgetState();
+}
+
+class _DeskDetailWidgetState extends State<DeskDetailWidget> {
+  Student? student;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _fetchStudent();
+  }
+
+  void _fetchStudent() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text(AppLocalizations.of(context)!.theme);
   }
 }
